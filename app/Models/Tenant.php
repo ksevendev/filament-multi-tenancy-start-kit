428864cc9b4512036a2e933cbc5e0adf5591c93c
felipewustarnold <felipearnold.94@gmail.com>
2024-11-23 12:10:14 -0300

    Atualizado layout e tela de empresa.

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -4,13 +4,16 @@ namespace App\Models;
 
 use App\Observers\TenantObserver;
 use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Grid;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Split;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\ToggleButtons;
 use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Support\Facades\Storage;
 use Leandrocfe\FilamentPtbrFormFields\Document;
 
@@ -34,14 +37,19 @@ class Tenant extends Model
         return $this->hasMany(Person::class);
     }
 
-    public function categories(): HasMany
+    public function addresses(): MorphMany
     {
-        return $this->hasMany(Category::class);
+        return $this->morphMany(Addresses::class, 'addressable');
     }
 
-    public function calendars(): HasMany
+    public function phones(): MorphMany
     {
-        return $this->hasMany(Calendar::class);
+        return $this->morphMany(Phones::class, 'phonable');
+    }
+
+    public function emails(): MorphMany
+    {
+        return $this->morphMany(Emails::class, 'emailable');
     }
 
     public function getFilamentAvatarUrl(): ?string
@@ -52,25 +60,64 @@ class Tenant extends Model
     public static function getForm(): array
     {
         return [
-            Section::make('Informações da empresa')
-                ->description('Dados gerais da empresa.')
+            Section::make('Informações da conta')
+                ->icon('heroicon-o-information-circle')
+                ->description('Gerencie as informações da sua conta')
                 ->schema([
-                    Split::make([
-                        Section::make([
-                            TextInput::make('name')
-                                ->label('Nome')
-                                ->required(),
-                            Document::make('document')
-                                ->label('CPF/CNPJ')
-                                ->required(),
-                            FileUpload::make('avatar')
-                                ->label('Avatar')
-                                ->acceptedFileTypes(['image/*'])
-                                ->rules(['image', 'max:1024']),
-                        ])->grow(false),
-                    ])->from('md'),
-                ]),
-
+                    TextInput::make('name')
+                        ->label('Nome')
+                        ->required()
+                        ->placeholder('Nome da empresa'),
+                    Document::make('document')
+                        ->label('CPF/CNPJ')
+                        ->placeholder('CPF/CNPJ da empresa'),
+                    ToggleButtons::make('type')
+                        ->label('Tipo')
+                        ->inline()
+                        ->grouped()
+                        ->options([
+                            'vehicle' => 'Veículo',
+                            'other'   => 'Outro',
+                        ])
+                        ->icons([
+                            'vehicle' => 'heroicon-o-truck',
+                            'other'   => 'heroicon-o-user',
+                        ])
+                        ->required(),
+                    TextInput::make('website')
+                        ->prefix('https://')
+                        ->label('Site'),
+                    FileUpload::make('avatar')
+                        ->label('Avatar')
+                        ->acceptedFileTypes(['image/*'])
+                        ->rules(['image', 'max:1024'])
+                        ->directory('avatars')
+                        ->downloadable()
+                        ->previewable(false),
+                ])
+                ->columns(),
+            Section::make('Contatos')
+                ->icon('heroicon-o-phone')
+                ->description('Insira os dados pessoais do cliente')
+                ->schema([
+                    Grid::make(2)
+                        ->schema(Phones::getForm())
+                        ->columnSpan(1),
+                    Grid::make(2)
+                        ->schema(Emails::getForm())
+                        ->columnSpan(1),
+                ])
+                ->grow(true)
+                ->columns(2),
+            Section::make('Endereços')
+                ->icon('heroicon-o-globe-americas')
+                ->description('Insira os dados pessoais do cliente')
+                ->schema([
+                    Grid::make(2)
+                        ->schema(Addresses::getForm())
+                        ->columnSpan(2),
+                ])
+                ->grow(true),
         ];
     }
 }
